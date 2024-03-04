425ea0ac6f2cfc88e2c075b2fd8bd64a308bb1c1
vaclavbohac <vaclav.bohac@productboard.com>
2024-03-04 23:18:55 +0100

    Use single event listenerer for prev & next buttons

--- public/app.js
+++ public/app.js
@@ -1,52 +1,31 @@
-const bckwBtnElm = document.querySelectorAll('.arrow-bckw-btn');
+(() => {
+  let active = 0;
 
-const fwdBtnElm = document.querySelectorAll('.arrow-fwd-btn');
+  const pages = document.querySelectorAll('.page');
 
-const page1Elm = document.querySelector('.page-1');
-const page2Elm = document.querySelector('.page-2');
-const page3Elm = document.querySelector('.page-3');
+  document.addEventListener('click', (evt) => {
+    const dir = evt.target.getAttribute('data-dir');
 
-const moveToPage1 = () => {
-  page1Elm.style.display = 'block';
-  page2Elm.style.display = 'none';
-  page3Elm.style.display = 'none';
-};
-
-const moveToPage2 = () => {
-  page1Elm.style.display = 'none';
-  page2Elm.style.display = 'block';
-  page3Elm.style.display = 'none';
-};
-
-const moveToPage3 = () => {
-  page1Elm.style.display = 'none';
-  page2Elm.style.display = 'none';
-  page3Elm.style.display = 'block';
-};
-
-bckwBtnElm.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    const movingToPage1 = e.target.classList.contains('arrow-bckw-btn-page2');
-    const movingToPage2 = e.target.classList.contains('arrow-bckw-btn-page3');
-
-    if (movingToPage1) {
-      moveToPage1();
-    } else if (movingToPage2) {
-      moveToPage2();
+    switch (dir) {
+      case 'next': {
+        active = Math.min(active + 1, pages.length - 1);
+        break;
+      }
+      case 'prev': {
+        active = Math.max(active - 1, 0);
+        break;
+      }
+      default:
+        // Random click on the website. Don't do anything.
+        return;
     }
+
+    pages.forEach((el, index) => {
+      if (index === active) {
+        el.classList.remove('hidden');
+      } else {
+        el.classList.add('hidden');
+      }
+    });
   });
-});
-
-fwdBtnElm.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    const movingToPage2 = e.target.classList.contains('arrow-fwd-btn-page1');
-
-    const movingToPage3 = e.target.classList.contains('arrow-fwd-btn-page2');
-
-    if (movingToPage2) {
-      moveToPage2();
-    } else if (movingToPage3) {
-      moveToPage3();
-    }
-  });
-});
+})();
