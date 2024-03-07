a00303f4bb9ea8eba9d5eb7b8dbbade8453fd0a1
Zuzana Belaskova <z.belaskova@gmail.com>
2024-03-07 17:55:23 +0100

    Modal for desktop

--- public/app.js
+++ public/app.js
@@ -1,17 +1,17 @@
 (() => {
   let active = 0;
 
-  const pages = document.querySelectorAll('.page');
+  const pages = document.querySelectorAll(".page");
 
-  document.addEventListener('click', (evt) => {
-    const dir = evt.target.getAttribute('data-dir');
+  document.addEventListener("click", (evt) => {
+    const dir = evt.target.getAttribute("data-dir");
 
     switch (dir) {
-      case 'next': {
+      case "next": {
         active = Math.min(active + 1, pages.length - 1);
         break;
       }
-      case 'prev': {
+      case "prev": {
         active = Math.max(active - 1, 0);
         break;
       }
@@ -22,10 +22,29 @@
 
     pages.forEach((el, index) => {
       if (index === active) {
-        el.classList.remove('hidden');
+        el.classList.remove("hidden");
       } else {
-        el.classList.add('hidden');
+        el.classList.add("hidden");
       }
     });
   });
 })();
+
+const modalElm = document.querySelector(".modal");
+const modalBtnElm = document.querySelectorAll(".open-modal");
+const closeModalElm = document.querySelector(".close");
+
+modalBtnElm.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    modalElm.style.display = "block";
+    console.log(modalElm.style.display);
+  });
+});
+
+const closeModal = () => {
+  modalElm.style.display = "none";
+};
+
+closeModalElm.addEventListener("click", () => closeModal());
+
+//how to close modal on escape? or on click outside of the modal?
