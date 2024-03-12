e868c7f83cb3866251cbccf58757c5ec3eeec8dd
vaclavbohac <vaclav.bohac@productboard.com>
2024-03-12 14:01:27 +0100

    Animated charts

--- public/app.js
+++ public/app.js
@@ -1,17 +1,45 @@
 (() => {
   let active = 0;
 
-  const pages = document.querySelectorAll(".page");
+  const pages = document.querySelectorAll('.page');
+  const charts = document.querySelectorAll('.animated-chart');
 
-  document.addEventListener("click", (evt) => {
-    const dir = evt.target.getAttribute("data-dir");
+  const animateChart = (chart) => {
+    const circle = chart.querySelector('.circle');
+    const r = circle.getAttribute('r');
+
+    const circleCircumference = 2 * Math.PI * r;
+    const targetDistance = circleCircumference * chart.getAttribute('data-value') / 100.0;
+
+    let currentDistance = 0;
+    const step = () => {
+      currentDistance += 2;
+
+      // Updates SVG circle with new angle
+      circle.setAttribute('stroke-dasharray', currentDistance + ', 2000');
+
+      // Animate till we reach the desired angle
+      if (currentDistance <= targetDistance) {
+        requestAnimationFrame(step);
+      }
+    };
+
+    requestAnimationFrame(step);
+  };
+
+  const animateCakeCharts = () => {
+    charts.forEach(animateChart);
+  };
+
+  document.addEventListener('click', (evt) => {
+    const dir = evt.target.getAttribute('data-dir');
 
     switch (dir) {
-      case "next": {
+      case 'next': {
         active = Math.min(active + 1, pages.length - 1);
         break;
       }
-      case "prev": {
+      case 'prev': {
         active = Math.max(active - 1, 0);
         break;
       }
@@ -22,29 +50,33 @@
 
     pages.forEach((el, index) => {
       if (index === active) {
-        el.classList.remove("hidden");
+        el.classList.remove('hidden');
       } else {
-        el.classList.add("hidden");
+        el.classList.add('hidden');
       }
     });
+
+    if (active === 1) {
+      animateCakeCharts();
+    }
   });
 })();
 
-const modalElm = document.querySelector(".modal");
-const modalBtnElm = document.querySelectorAll(".open-modal");
-const closeModalElm = document.querySelector(".close");
+const modalElm = document.querySelector('.modal');
+const modalBtnElm = document.querySelectorAll('.open-modal');
+const closeModalElm = document.querySelector('.close');
 
 modalBtnElm.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    modalElm.style.display = "block";
+  btn.addEventListener('click', () => {
+    modalElm.style.display = 'block';
     console.log(modalElm.style.display);
   });
 });
 
 const closeModal = () => {
-  modalElm.style.display = "none";
+  modalElm.style.display = 'none';
 };
 
-closeModalElm.addEventListener("click", () => closeModal());
+closeModalElm.addEventListener('click', () => closeModal());
 
-//how to close modal on escape? or on click outside of the modal?
+// how to close modal on escape? or on click outside of the modal?
